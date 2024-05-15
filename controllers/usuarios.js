414115de79652cf27fdc99bcb2fb99086a23bae8
Paul Pichon F <paul10_barca@hotmail.com>
2024-05-15 16:18:00 -0600

    Create: crear usuario y guardarlo en la BD

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,21 +1,38 @@
 // controllers usuarios
+// Modelo Usuario
+const Usuario = require("../models/usuario");
 
+//GET 
 const usuariosGet = (req, res) => {
+    // RESPUESTA
     res.json({
         msg: 'GET APP CONTROLLERS'
     });
 }
-const usuariosPost = (req, res) => {
+// POST
+const usuariosPost = async (req, res) => {
+    // body
+    const { nombre_completo, correo, password } = req.body;
+    // crear el usuario
+    const usuario = await Usuario({ nombre_completo, correo, password });
+    // Guardar el usuario en la BD
+    await usuario.save();
+    // RESPUESTA
     res.json({
-        msg: 'POST APP CONTROLLERS'
+        status: 200,
+        usuario
     });
 }
+// PUT
 const usuariosPut = (req, res) => {
+    // RESPUESTA
     res.json({
         msg: 'PUT APP CONTROLLERS'
     });
 }
+// DELETE
 const usuariosDelete = (req, res) => {
+    // RESPUESTA
     res.json({
         msg: 'DELETE APP CONTROLLERS'
     });
