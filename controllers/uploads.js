feefef50c0d8faa327007f5ae53c7cfde566bad1
Paul Pichon F <paul10_barca@hotmail.com>
2024-06-11 21:08:18 -0600

    Create: se crea funcion en carpeta helpers para poder subir archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,55 +1,23 @@
-// Path
-const path = require('path');
-// UUID
-const { v4: uuidv4 } = require('uuid');
+// subir archivos
+const { subirArchivo } = require("../helpers/subir-archivo");
 
 // funcion para cargar/subir imagenes
-const cargarArchivos = ( req, res ) => {
+const cargarArchivos = async ( req, res ) => {
     // verificar si vienen archivos y en caso de que no haya por lo menos uno muestra un error
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
         return res.status(400).json({msg: 'No hay archivos que subir.'});
     }
-    // verificar que si no viene nada en !req.files.archivo mostramos una alerta
-    // esto es lo que yo estoy esperando en mi backend --> req.files.archivo
-    // tambien se podria poner en el IF de arriba pero queda un poco mas visible lo que queremos de esta forma - pero es practicamente lo mismo
-    // if (!req.files.archivo ) {
-    //     return res.status(400).json({msg: 'No hay archivos que subir.'});
-    // }
-
-    // nombre de mi propiedad
-    const { archivo } = req.files;
-    // VALIDAR LA EXTENSION DEL ARCHIVO
-    // esto nos crea un array/arreglo
-    const nombreCortado = archivo.name.split('.');
-    // extension
-    // la extension seria la ultima posicion del arreglo nombreCortado
-    const extension = nombreCortado[ nombreCortado.length - 1];
-    
-    // VALIDAR LA EXTENSION DEL ARCHIVO
-    // extensiones validas
-    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
-    // validar estension
-    if ( !extensionesValidas.includes( extension ) ) {
-        // si la extension del archivo subido no esta dentro del arreglo, se muestra un error
-        return res.status( 400 ).json({
-            msg: `La extension ${ extension } no es permitida, ${ extensionesValidas }`
-        });
-    }
-
-    // UBICAR Y CAMBIAR EL NOMBRE DEL ARCHIVO
-    // Nombre del archivo mas la extension
-    // uuid() ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
-    const nombreTemp = `${ uuidv4() }.${ extension }`;
-
-    // nombre de la ruta
-    const uploadPath = path.join( __dirname, '../uploads/', nombreTemp );
-    // usar el metodo mv() para cambiar el archivo a cualquier parte de nuestro servidor
-    archivo.mv(uploadPath, (err) => {
-        // si hay algun error
-        if (err) return res.status(500).json({err});
-        // si todo sale bien
-        res.json({ msg: 'File uploaded! ' + uploadPath });
+    // llamamos la funcion para subir el archivo
+    // path completo del archivo
+    // archivo & extensiones & nombre de la carpeta
+    // se usan las extensiones por defecto y la carpeta seguira siendo la misma asi que no se manda nombre de carpeta
+    // nombre de la imagen
+    const nombre = await subirArchivo( req.files );
+    // respuesta
+    res.json({
+        nombre
     });
+    
 }
 // exports
 module.exports = {
---
+++ helpers/subir-archivo.js
@@ -0,0 +1,58 @@
+// Path
+const path = require('path');
+// UUID
+const { v4: uuidv4 } = require('uuid');
+
+// Hleper Subir archivo
+// como argumentos podemos recibir files 
+// se podria desestructurar directamente ({ archivo })
+// como segundo argumento ponemos las extensiones validas, de esta forma podriamos llamar la funcion subirArchivo y cambiar las extensiones si es que asi lo queremos por ejemplo si queremos validar archivos de texto .txt .pdf. etc etc
+// carpeta: la carpeta es donde se guardara el archivo subido
+const subirArchivo = ( files, extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'], carpeta = '' ) => {
+
+    return new Promise((resolve, reject ) => {
+
+        // verificar que si no viene nada en !req.files.archivo mostramos una alerta
+        // esto es lo que yo estoy esperando en mi backend --> req.files.archivo
+        // tambien se podria poner en el IF de arriba pero queda un poco mas visible lo que queremos de esta forma - pero es practicamente lo mismo
+        // if (!req.files.archivo ) {
+        //     return res.status(400).json({msg: 'No hay archivos que subir.'});
+        // }
+
+        // nombre de mi propiedad
+        const { archivo } = files;
+        // VALIDAR LA EXTENSION DEL ARCHIVO
+        // esto nos crea un array/arreglo
+        const nombreCortado = archivo.name.split('.');
+        // extension
+        // la extension seria la ultima posicion del arreglo nombreCortado
+        const extension = nombreCortado[ nombreCortado.length - 1];
+        
+        // VALIDAR LA EXTENSION DEL ARCHIVO
+        // validar estension
+        if ( !extensionesValidas.includes( extension ) ) {
+            // si la extension del archivo subido no esta dentro del arreglo, se muestra un error
+            return reject(`La extension ${ extension } no es permitida: ${ extensionesValidas }`);
+        }
+
+        // UBICAR Y CAMBIAR EL NOMBRE DEL ARCHIVO
+        // Nombre del archivo mas la extension
+        // uuid() ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+        const nombreTemp = `${ uuidv4() }.${ extension }`;
+
+        // nombre de la ruta
+        const uploadPath = path.join( __dirname, '../uploads/', carpeta, nombreTemp );
+        // usar el metodo mv() para cambiar el archivo a cualquier parte de nuestro servidor
+        archivo.mv(uploadPath, (err) => {
+            // si hay algun error
+            if (err) reject( err );
+            // si todo sale bien
+            resolve( nombreTemp );
+        });
+
+    });
+}
+// exports
+module.exports = {
+    subirArchivo
+}
