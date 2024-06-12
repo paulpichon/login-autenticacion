a05a2ef31e4ae9edfadbbda9c6be074384b2d5a7
Paul Pichon F <paul10_barca@hotmail.com>
2024-06-11 21:38:28 -0600

    Update: se añadio la forma de crear carpetas de destino en caso de que no exista

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -7,16 +7,23 @@ const cargarArchivos = async ( req, res ) => {
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
         return res.status(400).json({msg: 'No hay archivos que subir.'});
     }
-    // llamamos la funcion para subir el archivo
-    // path completo del archivo
-    // archivo & extensiones & nombre de la carpeta
-    // se usan las extensiones por defecto y la carpeta seguira siendo la misma asi que no se manda nombre de carpeta
-    // nombre de la imagen
-    const nombre = await subirArchivo( req.files );
-    // respuesta
-    res.json({
-        nombre
-    });
+    // El REJECT que devuelve el archivo subir-archivo.js nos devuelve un error que hace que reviente nuestra aplicacion en consola nos muestra un error muy feo, esto lo podemos manejar poniendo un try y un catch
+    try {
+        // llamamos la funcion para subir el archivo
+        // path completo del archivo
+        // ARGUMENTOS ---> archivo & extensiones & nombre de la carpeta
+        // se usan las extensiones por defecto y la carpeta seguira siendo la misma asi que no se manda nombre de carpeta
+        // nombre de la imagen
+        // mandar diferentes extensiones, se podria crear un arreglo con diferentes extensiones y mandarlo como argumento
+        // const nombre = await subirArchivo( req.files, ['txt', 'md'], 'textos' );
+        // en caso de que no mandemos algun argumento DEBEMOS poner UNDEFINED
+        const nombre = await subirArchivo( req.files, undefined, 'imgs' );
+        // respuesta
+        res.json({ nombre });
+
+    } catch (msg) {
+        return res.status(400).json({ msg });
+    }
     
 }
 // exports
