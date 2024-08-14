3583dfdffde4cc11bac9ec69039c79598559282c
Paul Pichon F <paul10_barca@hotmail.com>
2024-08-14 16:13:14 -0600

    Update: se añade el campo fecha_actualizacion para cuando se haga un PUT al post

--- models/posteo.js
+++ models/posteo.js
@@ -3,29 +3,38 @@
 const {Schema, model} = require('mongoose');
 
 const PosteosSchema = Schema({
+    // ID del usuario, este se crea al validaar el TOKEN, ahi viene el UID del usuario
     _idUsuario: {
         type: Schema.Types.ObjectId,
         ref: 'Usuario',
         required: true
     },
+    // Texto que acompaña al POSTEO
     texto: {
         type: String,
         required: [true, 'El texto del post es obligatorio']
     }, 
+    // IMG del posteo, cabe resaltar que solo se puede subir una sola imagen
     img: {
         type: String,
         required: [ true, 'La imagen es obligatoria']
     },
+    // Fecha de creacion del POSTEO
     fecha_creacion: {
         type: Date,
         default: Date.now
+    },
+    // Fecha de actualizacion
+    fecha_actualizacion: {
+        type: Date,
     }
-
 });
 // 
 PosteosSchema.methods.toJSON = function() {
     // destructuring
-    const { __v, ...posteo } = this.toObject();
+    const { _id, __v, ...posteo } = this.toObject();
+    // cambiar vizualmente _id po idPosteo
+    posteo.idPost = _id;
     // regresar los demas valores de posteo
     return posteo;
 }
