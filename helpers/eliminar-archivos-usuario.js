6a3c326b93407bd3a8823b38986907c391bdd56f
Paul Pichon F <paul10_barca@hotmail.com>
2024-08-13 21:41:23 -0600

    Update: Se modifico el nombre de la carpeta de imagen_perfil_usuarios

--- helpers/eliminar-archivos-usuario.js
+++ helpers/eliminar-archivos-usuario.js
@@ -16,7 +16,7 @@ const eliminarArchivosUsuario = async ( id ) => {
     if ( imagen_perfil !== 'assets/no-image.jpg' ) {
         // si existe hay que borrar la imagen del servidor
         // se construye el path de la imagen a borrar
-        const pathImagen = path.join(__dirname, '../uploads/imagen-perfil-usuarios/',imagen_perfil);
+        const pathImagen = path.join(__dirname, '../uploads/imagen_perfil_usuarios/',imagen_perfil);
         // verificar si existe la imagen fisicamente
         if ( fs.existsSync( pathImagen )) {
             // si existe la imagen, la borramos
