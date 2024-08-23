bf546530fef006fabf1a494fc65dca6ea9bcd83a
Paul Pichon F <paul10_barca@hotmail.com>
2024-08-23 13:09:23 -0600

    Create: creacion de API para obtener los usuarios que dieron LIKE a determinado posteo mediante su ID

--- controllers/posteos.js
+++ controllers/posteos.js
@@ -19,94 +19,106 @@ const { subirArchivo } = require("../helpers/subir-archivo");
 // Este GET es para mostrar al usuario que inicia sesion todos los POSTEOS de todos los usuarios
 //Pero se limita a mostrar 15 POSTEOS
 const posteosGet = async (req, res) => {
-    // Query paramaeters
-    // Obtenemos la pagina y el limite
-    // Parametros de limitacion de posteos
-    const { page = 1, limite = 15} = req.query;
-    // DE MOMENTO ESTA API NO SE VA A OCUPAR
-    //Mostrando el total(numero) y los posteos de usuarios 
-    const [ total_registros, posteos ] = await Promise.all([
-        // Conteo de posteos
-        Posteo.countDocuments(),
-        // Buscar todos los posteos y traer lo ultimos 15 posteos creados por usuarios
-        Posteo.find()
-        //se muestra pagina page menos 1 por el limite 
-        .skip( (+page - 1) * +limite )
-        // Limitamos los registros a 15
-        .limit( +limite )
-        //Traemos los ULTIMOS registros de esta forma { _id : -1 } puede ser por el ID o por fecha_creacion
-        // Orden descendente
-        // .sort( { _id : -1} ) 
-        // { fecha_creacion : -1}
-        // tanto por _id o por fecha_creacion se puede usar -1
-        .sort( { fecha_creacion : -1} ) 
-        
-    ]);
+    try {
+        // Query paramaeters
+        // Obtenemos la pagina y el limite
+        // Parametros de limitacion de posteos
+        const { page = 1, limite = 15} = req.query;
+        // DE MOMENTO ESTA API NO SE VA A OCUPAR
+        //Mostrando el total(numero) y los posteos de usuarios 
+        const [ total_registros, posteos ] = await Promise.all([
+            // Conteo de posteos
+            Posteo.countDocuments(),
+            // Buscar todos los posteos y traer lo ultimos 15 posteos creados por usuarios
+            Posteo.find()
+            //se muestra pagina page menos 1 por el limite 
+            .skip( (+page - 1) * +limite )
+            // Limitamos los registros a 15
+            .limit( +limite )
+            //Traemos los ULTIMOS registros de esta forma { _id : -1 } puede ser por el ID o por fecha_creacion
+            // Orden descendente
+            // .sort( { _id : -1} ) 
+            // { fecha_creacion : -1}
+            // tanto por _id o por fecha_creacion se puede usar -1
+            .sort( { fecha_creacion : -1} ) 
+            
+        ]);
 
-    // RESPUESTA
-    res.json({
-        page,
-        next: `/api/posteos?page=${ (+page + 1)}&limite=${ +limite }`,
-        prev: (+page - 1 ) > 0 ? `/api/posteos?page=${ (+page - 1)}&limite=${ +limite }`: null,
-        limite,
-        total_registros,
-        mostrando: posteos.length,
-        posteos
-    });
+        // RESPUESTA
+        res.json({
+            page,
+            next: `/api/posteos?page=${ (+page + 1)}&limite=${ +limite }`,
+            prev: (+page - 1 ) > 0 ? `/api/posteos?page=${ (+page - 1)}&limite=${ +limite }`: null,
+            limite,
+            total_registros,
+            mostrando: posteos.length,
+            posteos
+        });
+    } catch (error) {
+        res.status(400).json({ msg: 'Hubo un error al obtener los posteos de usuarios, contactar a soporte.'});
+    }
 }
 // Get - Mostrar un solo posteo
 const posteoGet = async ( req, res) => {
-    // Obtener el ID del posts
-    const { id } = req.params;
-    // Buscar el ID del post en la BD
-    const posteo = await Posteo.findById( id )
-    // populate() sirve para mostrar informacion completa en base a la propiedad a la que se haga señalamiento en este caso nos muestra la informacion a partir de _idUsuario
-        // .populate( '_idUsuario' )
-        // traemos infORMACION solo la necesaria: 'nombre_completo imagen_perfil url'
-        .populate( '_idUsuario', 'nombre_completo imagen_perfil url' )
-    // RESPUESTA
-    res.json({
-        posteo
-    });
+    try {
+        // Obtener el ID del posts
+        const { id } = req.params;
+        // Buscar el ID del post en la BD
+        const posteo = await Posteo.findById( id )
+        // populate() sirve para mostrar informacion completa en base a la propiedad a la que se haga señalamiento en este caso nos muestra la informacion a partir de _idUsuario
+            // .populate( '_idUsuario' )
+            // traemos infORMACION solo la necesaria: 'nombre_completo imagen_perfil url'
+            .populate( '_idUsuario', 'nombre_completo imagen_perfil url' )
+        // RESPUESTA
+        res.json({
+            posteo
+        });
+    } catch (error) {
+        res.status(400).json({ msg: 'Hubo un error al obtener el posteo por ID, contactar a soporte.'});
+    }
 }
 // Get - Mostrar todos los posteos de un solo usuario mediante su ID
 const posteosUsuarioGet = async ( req, res) => {
-    // obtener el ID del usuario
-    const { idUsuario } = req.params;
-    // Query con la que hacemos el filtro
-    const filtroIdUsuario = { _idUsuario: idUsuario };
-    // Query paramaeters
-    // Obtenemos la pagina y el limite
-    // Parametros de limitacion de posteos
-    const { page = 1, limite = 15} = req.query;
-    // Hacer la consulta con Promise.all
-    const [ total_registros, posteos ] = await Promise.all([
-        // Conteo de posteos
-        Posteo.countDocuments( filtroIdUsuario ),
-        // buscar los documentos en la BD
-        Posteo.find( filtroIdUsuario )
-         //Traemos los ULTIMOS registros de esta forma { _id : -1 } puede ser por el ID o por fecha_creacion
-        // Orden descendente
-        // .sort( { _idUsuario : -1} ) 
-        // { fecha_creacion : -1}
-        // tanto por _idUsuario o por fecha_creacion se puede usar -1
-        .sort( { fecha_creacion: -1} ) 
-        //se muestra pagina page menos 1 por el limite 
-        .skip( (+page - 1) * +limite )
-        // Limitamos los registros a 15
-        .limit( parseInt(limite) )
-        
-    ]);
-    // RESPUESTA
-    res.json({
-        page,
-        next: `/api/posteos/usuario/${ idUsuario }?page=${ (+page + 1)}&limite=${ +limite }`,
-        prev: (+page - 1 ) > 0 ? `/api/posteos/usuario/${ idUsuario }?page=${ (+page - 1)}&limite=${ +limite }`: null,
-        limite,
-        total_registros,
-        mostrando: posteos.length,
-        posteos
-    });
+    try {
+        // obtener el ID del usuario
+        const { idUsuario } = req.params;
+        // Query con la que hacemos el filtro
+        const filtroIdUsuario = { _idUsuario: idUsuario };
+        // Query paramaeters
+        // Obtenemos la pagina y el limite
+        // Parametros de limitacion de posteos
+        const { page = 1, limite = 15} = req.query;
+        // Hacer la consulta con Promise.all
+        const [ total_registros, posteos ] = await Promise.all([
+            // Conteo de posteos
+            Posteo.countDocuments( filtroIdUsuario ),
+            // buscar los documentos en la BD
+            Posteo.find( filtroIdUsuario )
+            //Traemos los ULTIMOS registros de esta forma { _id : -1 } puede ser por el ID o por fecha_creacion
+            // Orden descendente
+            // .sort( { _idUsuario : -1} ) 
+            // { fecha_creacion : -1}
+            // tanto por _idUsuario o por fecha_creacion se puede usar -1
+            .sort( { fecha_creacion: -1} ) 
+            //se muestra pagina page menos 1 por el limite 
+            .skip( (+page - 1) * +limite )
+            // Limitamos los registros a 15
+            .limit( parseInt(limite) )
+            
+        ]);
+        // RESPUESTA
+        res.json({
+            page,
+            next: `/api/posteos/usuario/${ idUsuario }?page=${ (+page + 1)}&limite=${ +limite }`,
+            prev: (+page - 1 ) > 0 ? `/api/posteos/usuario/${ idUsuario }?page=${ (+page - 1)}&limite=${ +limite }`: null,
+            limite,
+            total_registros,
+            mostrando: posteos.length,
+            posteos
+        });
+    } catch (error) {
+        res.status(400).json({ msg: 'Hubo un error al obtener los Posteos, contactar a soporte.'});
+    }
 }
 // POST - Crear un POSTEO
 const posteosPost = async (req, res) => {
@@ -148,42 +160,50 @@ const posteosPost = async (req, res) => {
 }
 // PUT - ACTUALIZAR UNA PUBLICACION * Verificar si es necesario este endpoint
 const posteosPut = async (req, res) => {
-    // ID del posteo
-    const { id } = req.params;
-    // body
-    const{ texto } = req.body;
-    // buscamos el posteo por el ID
-    // Se actualiza el texto del  posteo y la fecha_actualizacion
-    const posteo = await Posteo.findByIdAndUpdate( id, { texto, fecha_actualizacion: Date.now() }, {
-        new: true
-    });
-    // RESPUESTA
-    res.json({
-        posteo
-    });
+    try {
+        // ID del posteo
+        const { id } = req.params;
+        // body
+        const{ texto } = req.body;
+        // buscamos el posteo por el ID
+        // Se actualiza el texto del  posteo y la fecha_actualizacion
+        const posteo = await Posteo.findByIdAndUpdate( id, { texto, fecha_actualizacion: Date.now() }, {
+            new: true
+        });
+        // RESPUESTA
+        res.json({
+            posteo
+        });
+    } catch (error) {
+        res.status(400).json({ msg: 'Hubo un error al actualizar el Posteo, contactar a soporte.'});
+    }
 }
 // DELETE - BORRAR UNA PUBLICACION
 const posteosDelete = async (req, res) => {
-    // Obtener el ID del posteo
-    const { id } = req.params;
-    // buscar en la BD la ruta de la imagen
-    const { img } = await Posteo.findById( id );
-    // verificar si existe img en la BD
-    if ( img ) {
-        // si existe hay que borrar la imagen del servidor
-        // se construye el path de la imagen a borrar
-        const pathImagen = path.join(__dirname, '../uploads/posteos_usuarios/', img);
-        // verificar si existe la imagen fisicamente
-        if ( fs.existsSync( pathImagen )) {
-            // si existe la imagen, la borramos
-            fs.unlinkSync( pathImagen );
+    try {
+        // Obtener el ID del posteo
+        const { id } = req.params;
+        // buscar en la BD la ruta de la imagen
+        const { img } = await Posteo.findById( id );
+        // verificar si existe img en la BD
+        if ( img ) {
+            // si existe hay que borrar la imagen del servidor
+            // se construye el path de la imagen a borrar
+            const pathImagen = path.join(__dirname, '../uploads/posteos_usuarios/', img);
+            // verificar si existe la imagen fisicamente
+            if ( fs.existsSync( pathImagen )) {
+                // si existe la imagen, la borramos
+                fs.unlinkSync( pathImagen );
+            }
         }
+        // llamar la funcion para borrar el archivo que se subio en POSTEO
+        // buscar el id en la base de datos y borrarlo    
+        const posteo = await Posteo.findByIdAndDelete( id );
+        // RESPUESTA
+        res.json( posteo );
+    } catch (error) {
+        res.status(400).json({ msg: 'Hubo un error al eliminar el Posteo, contactar a soporte.'});
     }
-    // llamar la funcion para borrar el archivo que se subio en POSTEO
-    // buscar el id en la base de datos y borrarlo    
-    const posteo = await Posteo.findByIdAndDelete( id );
-    // RESPUESTA
-    res.json( posteo );
 }
 // Funcion para dar LIKES a POSTEOS
 const putLikePosteo = async (req, res) => { 
@@ -218,7 +238,7 @@ const putLikePosteo = async (req, res) => {
         res.status(400).json({ msg: 'Hubo un error al generar el LIKE, contactar a soporte.'});
     }
 }
-// 
+// Obtener la cantidad de likes de una publicacion
 const getLikesPosteos = async (req, res) => {
     try {
         // obtener ID de publicacion
@@ -234,6 +254,20 @@ const getLikesPosteos = async (req, res) => {
         });
     }
 }
+// Obtener los usuarios que le dieron like a una publicacion mediante su id de posteo
+const getLikesUsuariosPosteos = async (req, res) => {
+    try {
+        // obtener ID de publicacion
+        const { id } = req.params;
+        // buscar en la BD
+        const likes = await Like.find({ _idPosteo: id })
+            .populate('_idUsuario', 'nombre_completo imagen_perfil');
+        // Respuesta
+        res.json(likes);
+    } catch (err) {
+        res.status(400).send('Hubo un error al obtener los usuarios que dieron like, favor de contactar a soporte');
+    }
+}
 // exports
 module.exports = {
     posteosGet,
@@ -243,5 +277,6 @@ module.exports = {
     posteosPut,
     posteosDelete,
     putLikePosteo,
-    getLikesPosteos
+    getLikesPosteos,
+    getLikesUsuariosPosteos
 }
